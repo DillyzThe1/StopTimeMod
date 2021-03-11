e1d76244a09fa921b52e39319df2622bf9b91347
DillyzThe1 <34419642+DillyzThe1@users.noreply.github.com>
2021-03-10 19:02:11 -0600

    added cheepyt servers lol

--- Source Code/HarmonyMain.cs
+++ Source Code/HarmonyMain.cs
@@ -4,6 +4,9 @@ using HarmonyLib;
 using Reactor;
 using Essentials.Options;
 using System;
+using BepInEx.Configuration;
+using System.Linq;
+using System.Net;
 
 namespace StopTime
 {
@@ -16,9 +19,29 @@ namespace StopTime
         public Harmony Harmony { get; } = new Harmony(Id);
         public static CustomNumberOption freezeCooldown = CustomOption.AddNumber("Freeze Button Cooldown", 10f, 15f, 60f, 2.5f);
         public static CustomNumberOption freezeTimer = CustomOption.AddNumber("Freeze Time Active", 20f, 15f, 45f, 2.5f);
+        public ConfigEntry<string> Name { get; set; }
+        public ConfigEntry<string> Ip { get; set; }
+        public ConfigEntry<ushort> Port { get; set; }
         public override void Load()
         {
             Harmony.PatchAll();
+            Name = Config.Bind("Server", "Name", "CheepYT - EU");
+            Ip = Config.Bind("Server", "Ipv4 or Hostname", "207.180.234.175");
+            Port = Config.Bind("Server", "Port", (ushort)22023);
+            var defReg = AOBNFCIHAJL.DefaultRegions.ToList();
+            if (Uri.CheckHostName(Ip.Value).ToString() == "Dns")
+            {
+                foreach (IPAddress adress in Dns.GetHostAddresses(Ip.Value))
+                {
+                    if (adress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        Ip.Value = adress.ToString();
+                        break;
+                    }
+                }
+            }
+            defReg.Insert(0, new OIBMKGDLGOG(Name.Value, Ip.Value, new[] { new PLFDMKKDEMI($"{Name.Value}-Master-1", Ip.Value, Port.Value) }));
+            AOBNFCIHAJL.DefaultRegions = defReg.ToArray();
         }
     }
 }
